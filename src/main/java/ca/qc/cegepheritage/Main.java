db02a9517ca14b3aa7fea3f5cb19de6ac388583b
Jeremy <2551729@cegep-heritage.qc.ca>
2026-04-10 11:39:23 -0400

    Modify files

--- src/main/java/ca/qc/cegepheritage/Main.java
+++ src/main/java/ca/qc/cegepheritage/Main.java
@@ -4,6 +4,7 @@ package ca.qc.cegepheritage;
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-
+        System.out.println("Why is this so cooked");
+        System.out.println("WHAT WILL YOU HAVE AFTER 5000 YEARS!!?!?!?!?");
     }
 }
--- src/test/java/ca/qc/cegepheritage/TreeTest.java
+++ src/test/java/ca/qc/cegepheritage/TreeTest.java
@@ -24,8 +24,14 @@ class TreeTest {
     }
     @Test
     public void testGetInfoReturnsCorrectString() {
-        TestTree tree = new TestTree("Tree",2,2);
-        assertEquals("Tree has been in the garden for 2 days and is 2 years old.",tree.getInfo());
+        TestTree tree = new TestTree("Tree",3,2);
+        assertEquals("Tree has been in the garden for 3 days and is 2 years old.",tree.getInfo());
+    }
+
+    @Test
+    public void testGetTreeReturnsFalseWhenNotInstanceOfHerb() {
+        TestTree tree = new TestTree("tree", 2, 2);
+        assertFalse(1 == 2);
     }
 
 
